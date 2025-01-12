f7061697b023390a90b65c4a82b2ac6a42cb11a2
Saqib Ayoub <saqibtelixy@gmail.com>
2025-01-12 23:19:55 +0530

    add messages functionality v 1.26.52

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 dotenv.config();
@@ -9,6 +10,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
