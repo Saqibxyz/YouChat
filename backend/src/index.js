2d14e040be1586a0d0d22a464db425fac2b88975
Saqib Ayoub <saqibtelixy@gmail.com>
2025-01-19 23:05:15 +0530

    fix cors. v.1.49.35

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    Credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
