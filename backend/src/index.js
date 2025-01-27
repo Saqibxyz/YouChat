b2ebb8fbed1fbf21a7e36cf0494231ff2dbdf4b6
Saqib Ayoub <saqibtelixy@gmail.com>
2025-01-27 12:41:54 +0530

    add Message chat feature v.3.29.30

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,7 +7,8 @@ export const useAuthStore = create((set) => ({
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
-  isCheckingAuth: true,
+    isCheckingAuth: true,
+  onlineUsers:[],
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
