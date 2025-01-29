0d119dafe20a84b3a6c43b1e94416241f729a121
Saqib Ayoub <saqibtelixy@gmail.com>
2025-01-29 12:13:52 +0530

    change development mode to production

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { app,server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 dotenv.config();
 
 const port = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -19,6 +21,13 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
