3aec4641a520b5652cdfe17ebcaeee332f6a114c
Saqib Ayoub <saqibtelixy@gmail.com>
2025-01-29 11:47:22 +0530

    increase database connect time

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("failed to connect", error);
+    console.error("Failed to connect", error);
+    setTimeout(connectDB, 5000); // Retry after 5 seconds
   }
 };
